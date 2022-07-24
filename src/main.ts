968b0cf603d78714eef7967b9623d654c8291f9e
abdelkader202104 <abdelkader.mokhtar.sakhi@gmail.com>
2022-07-24 18:09:39 +0200

    SwaggerModule

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('Nest Learning MySQL Unit TESTING')
-  .setVersion('1.0')
-  .build();
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
+    .setTitle('Cats example')
+    .setDescription('Nest Learning MySQL Unit TESTING')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
   await app.listen(3000);
 }
 bootstrap();
